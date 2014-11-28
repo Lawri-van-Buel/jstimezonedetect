758e34c5c37f6dbb0e82a19ab994eb94eb9da627
Jon Nylander <jon.nylander@projectplace.com>
2014-11-28 12:02:02 +0100

    Several changes in build
     * Now packaging as CommonJS in gulp build
     * Resulting files are now found in dist folder
     * Main logic found in jstz.main.js
     * Generated rules found in jstz.rules.js

--- gulpfile.js
+++ gulpfile.js
@@ -3,9 +3,14 @@ var uglify = require('gulp-uglify');
 var jshint = require('gulp-jshint');
 var concat = require('gulp-concat');
 var header = require('gulp-header');
-var package = require('./package.json')
+var footer = require('gulp-footer');
+var commonjs = require('gulp-wrap-commonjs');
+var rename = require('gulp-rename');
+var del = require('del');
 
-var buildTime = (function () {
+var pkg = require('./package.json');
+
+var buildTime = function () {
     var now = new Date();
     var getMonth = function () {
         if (now.getMonth() < 10) {
@@ -22,20 +27,50 @@ var buildTime = (function () {
     };
 
     return now.getFullYear() + "-" + getMonth() + "-" + getDate()
-}());
-
-var paths = {
-    scripts: ['jstz.js','rules.js']
 };
 
-gulp.task('build', [], function () {
+var paths = {
+    scripts: ['jstz.*.js']
+};
+
+gulp.task('jshint', [], function () {
     return gulp.src(paths.scripts)
         .pipe(jshint())
-        .pipe(jshint.reporter('default'))
+        .pipe(jshint.reporter('default'));
+});
+
+gulp.task('concat', ['jshint'], function () {
+    return gulp.src(paths.scripts)
+        .pipe(concat('jstz.concat.js'))
+        .pipe(gulp.dest('dist'))
+});
+
+gulp.task('common-js', ['concat'], function () {
+    return gulp.src(['dist/jstz.concat.js'])
+        .pipe(header('(function (root) {'))
+        .pipe(footer("\n" +
+        "    if (typeof exports !== 'undefined') {\n" +
+        "        exports.jstz = jstz;\n" +
+        "    } else {\n" +
+        "        root.jstz = jstz;\n" +
+        "    }\n" +
+        "})(this);\n"))
+        .pipe(rename('jstz.js'))
+        .pipe(gulp.dest('dist'))
+});
+
+gulp.task('uglify', ['common-js'], function () {
+    return gulp.src(['dist/jstz.js'])
         .pipe(uglify())
-        .pipe(concat('jstz.min.js'))
-        .pipe(header('/*! jsTimezoneDetect - v' + package.version + ' - ' + buildTime + ' */\n'))
-        .pipe(gulp.dest('.'))
+        .pipe(rename('jstz.min.js'))
+        .pipe(header('/* jstz.min.js Version: ' + pkg.version + ' Build date: ' + buildTime() + ' */\n'))
+        .pipe(gulp.dest('dist'))
+});
+
+gulp.task('build', ['uglify'], function () {
+    return del([
+        'dist/jstz.co*.js'
+    ]);
 });
 
 gulp.task('watch', function () {
